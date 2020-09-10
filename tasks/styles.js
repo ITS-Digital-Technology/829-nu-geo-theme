d6372cf87eb45838668983a9f9ac0cbcf2a248a2
Storm Rockwell <storm@829llc.com>
2020-09-10 18:13:48 -0400

    Add mixin for icon font and fix styles compiler

--- tasks/styles.js
+++ tasks/styles.js
@@ -5,15 +5,17 @@ const gulpSass = require('gulp-sass');
 const path = require('path');
 
 const srcThemeDir = path.resolve( config.paths.srcRoot + config.paths.themes + config.paths.themeName + '/' );
+const srcRoot = path.resolve( config.paths.srcRoot );
 const iconfontScss = `${srcThemeDir}${config.paths.iconfont.scss}/_iconfont.scss`;
 
-gulp.task('styles', () => {
-    return gulp.src([`${config.srcRoot}/**/*.{sass,scss}`])
+gulp.task('styles', (done) => {
+    gulp.src([`${srcRoot}/**/*.{sass,scss}`])
         .pipe(gulpSass({
             outputStyle: config.isProduction ? 'compressed' : 'expanded',
             precision: 8
         }).on('error', gulpSass.logError))
-        .pipe(gulp.dest('web'));
+        .pipe(gulp.dest('web/app'))
+        .on('finish', done);
 });
 
 gulp.task('styles:watch', () => {
