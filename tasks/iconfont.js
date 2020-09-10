a66a33bbc1deb072974e0e04d46f8a818d095485
Storm Rockwell <storm@829llc.com>
2020-09-10 17:52:30 -0400

    Add base theme icons

--- tasks/iconfont.js
+++ tasks/iconfont.js
@@ -3,12 +3,13 @@ const config = require('../gulp.config');
 
 const iconfont = require('gulp-iconfont');
 const runTimestamp = Math.round(Date.now()/1000);
+const async = require('async');
 const consolidate = require('gulp-consolidate');
 
 const srcThemeDir = config.paths.srcRoot + config.paths.themes + config.paths.themeName + '/';
 const webThemeDir = config.paths.webRoot + config.paths.themes + config.paths.themeName + '/';
 
-gulp.task('iconfont', function(done) {
+gulp.task('iconfont', function(done){
     var iconStream = gulp.src([config.paths.iconfont.src])
         .pipe(iconfont({
             fontName: 'iconfont-' + config.paths.themeName,
@@ -19,28 +20,26 @@ gulp.task('iconfont', function(done) {
             timestamp: runTimestamp, // recommended to get consistent builds when watching files
         }));
 
-    gulp.parallel([
+    async.parallel([
         function handleGlyphs (cb) {
-            iconStream.on('glyphs', function(glyphs, options) {
-                gulp.src(srcThemeDir +config.paths.iconfont.template)
-                .pipe(consolidate('lodash', {
-                    glyphs: glyphs,
-                    fontCacheBuster: Math.random().toString(36).substring(7),
-                    fontName: 'iconfont-' + config.paths.themeName,
-                    themePath: '/' + config.paths.themes + config.paths.themeName + '/',
-                    fontPath: config.paths.fonts.dist + '/iconfont-' + config.paths.themeName,
-                    className: config.paths.iconfont.class
-                }))
-                .pipe(gulp.dest(srcThemeDir + config.paths.iconfont.scss))
-                .on('finish', cb);
-            });
+        iconStream.on('glyphs', function(glyphs, options) {
+            gulp.src(srcThemeDir +config.paths.iconfont.template)
+            .pipe(consolidate('lodash', {
+                glyphs: glyphs,
+                fontCacheBuster: Math.random().toString(36).substring(7),
+                fontName: 'iconfont-' + config.paths.themeName,
+                themePath: '/' + config.paths.themes + config.paths.themeName + '/',
+                fontPath: config.paths.fonts.dist + '/iconfont-' + config.paths.themeName,
+                className: config.paths.iconfont.class
+            }))
+            .pipe(gulp.dest(srcThemeDir + config.paths.iconfont.scss))
+            .on('finish', cb);
+        });
         },
         function handleFonts (cb) {
-            iconStream
-                .pipe(gulp.dest((webThemeDir + config.paths.fonts.dist + '/iconfont-' + config.paths.themeName)))
-                .on('finish', cb);
+        iconStream
+            .pipe(gulp.dest((webThemeDir + config.paths.fonts.dist + '/iconfont-' + config.paths.themeName)))
+            .on('finish', cb);
         }
-    ]);
-
-    done();
+    ], done);
 });
