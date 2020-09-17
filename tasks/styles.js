7b5ac30027f08d69af2548f1ce7b4b7b4d227542
Storm Rockwell <storm@829llc.com>
2020-09-17 09:21:54 -0400

    Remove extra slash

--- tasks/styles.js
+++ tasks/styles.js
@@ -5,7 +5,7 @@ const gulpSass = require('gulp-sass');
 const path = require('path');
 
 gulp.task('styles', (done) => {
-    gulp.src([`${config.paths.srcRoot}/**/*.{sass,scss}`])
+    gulp.src([`${config.paths.srcRoot}**/*.{sass,scss}`])
         .pipe(gulpSass({
             outputStyle: config.isProduction ? 'compressed' : 'expanded',
             precision: 8
@@ -16,7 +16,7 @@ gulp.task('styles', (done) => {
 
 gulp.task('styles:watch', () => {
     gulp.watch(
-        [`${config.paths.srcRoot}/**/*.{scss, sass}`], 
+        [`${config.paths.srcRoot}**/*.{scss, sass}`], 
         gulp.series('styles')
     );
 });
