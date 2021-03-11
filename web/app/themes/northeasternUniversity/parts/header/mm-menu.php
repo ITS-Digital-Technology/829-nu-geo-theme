c80c6f9a4e62d055935f0cd79106225396b25e4f
Artur Oberski <artur.oberski@dev-chop-chop.org>
2021-03-11 16:35:04 +0100

    Fix nav menu list

--- web/app/themes/northeasternUniversity/parts/header/mm-menu.php
+++ web/app/themes/northeasternUniversity/parts/header/mm-menu.php
@@ -1,24 +1,25 @@
 <?php
-$menu_id = isset($menu_id) ? $menu_id : get_sub_field('mm_wp_menu');
-$menus = wp_get_nav_menu_items($menu_id);
+$menu_id = isset( $menu_id ) ? $menu_id : get_sub_field( 'mm_wp_menu' );
+$items   = wp_get_nav_menu_items( $menu_id );
 
-if (!empty($menus)) :
+if ( ! empty( $items ) ) :
 ?>
-<ul id="menu-programs-menu-1" class="menu">
-    <?php foreach($menus as $menu) : ?>
-    <li id="menu-item-715" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-715 <?php echo !empty($menu->post_content) ? 'menu-item--has-description' : ''; ?>">
-        <a href="<?php echo get_the_permalink($menu->ID); ?>">
-            <p class="menu-item__title">
-                <?php echo $menu->post_title; ?>
-            </p>
-            <?php if (!empty($menu->post_content)) : ?>
-            <p class="menu-item__description">
-                <?php echo $menu->post_content; ?>
-            </p>
-            <?php endif; ?>
-        </a>
-    </li>
-    <?php endforeach; ?>
+<ul id="menu-<?php echo $menu_id; ?>" class="menu">
+<?php foreach ( $items as $item ) : ?>
+	<li id="menu-item-<?php echo $item->object_id; ?>" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-<?php echo $item->object_id; ?> <?php echo ! empty( $item->description ) ? 'menu-item--has-description' : ''; ?>">
+		<a href="<?php echo get_the_permalink( $item->object_id ); ?>">
+			<p class="menu-item__title">
+				<?php echo $item->title; ?>
+			</p>
+
+		<?php if ( ! empty( $item->description ) ) : ?>
+			<p class="menu-item__description">
+				<?php echo $item->description; ?>
+			</p>
+		<?php endif; ?>
+		</a>
+	</li>
+<?php endforeach; ?>
 </ul>
 <?php
 endif;
