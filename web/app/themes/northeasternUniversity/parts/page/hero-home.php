676cc5dd18b99d26838ca558d0d1fa140d364a6b
Krzysztof Olszowy <krzysztof.olszowy@chop-chop.org>
2021-03-16 13:01:31 +0100

    add hero background video functionality

--- web/app/themes/northeasternUniversity/parts/page/hero-home.php
+++ web/app/themes/northeasternUniversity/parts/page/hero-home.php
@@ -9,21 +9,36 @@
 
 
 $title         = isset( $title ) ? $title : '';
-$thumbnail      = isset( $thumbnail ) ? $thumbnail : false;
+$thumbnail     = isset( $thumbnail ) ? $thumbnail : false;
 $block_class   = isset( $block_class ) ? $block_class : false;
 $block_class[] = 'page-hero-home';
-$gradient      = get_field('hero_display_gradient');
+$gradient      = get_field( 'hero_display_gradient' );
+$bg_video      = isset( $bg_video ) ? $bg_video : false;
 
-if( $gradient ){
-    $block_class[] = 'page-hero--gradient';
+if ( $gradient ) {
+	$block_class[] = 'page-hero--gradient';
 }
-if( $title ){
-    $block_class[] = 'page-hero--text';
+if ( $title ) {
+	$block_class[] = 'page-hero--text';
+}
+if ( $bg_video ) {
+	$block_class[] = 'page-hero--video';
 }
 
 ?>
 <section class="<?php echo implode( ' ', $block_class ); ?>">
 	<div class="page-hero-home__wrapper">
+		<?php
+		if ( ! empty( $bg_video ) ) {
+			get_theme_part(
+				'elements/video-background',
+				array(
+					'video' => $bg_video,
+
+				)
+			);
+		}
+		?>
 		<?php if ( ! empty( $title ) ) : ?>
 		<div class="container">
 			<div class="row page-hero__row">
@@ -41,7 +56,7 @@ if( $title ){
 	</div>
 <?php
 if ( is_front_page() ) {
-	get_theme_part( 'elements/program-filters/filters',['mobile_modal'=>true] );
+	get_theme_part( 'elements/program-filters/filters', [ 'mobile_modal' => true ] );
 }
 ?>
 </section>
