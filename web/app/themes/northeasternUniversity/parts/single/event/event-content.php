77e4b5a49bac0991c34c474248c4183574b3ee3f
Chris <therstyle@gmail.com>
2021-04-09 20:27:03 -0400

    Fixed the_content() - https://829.clickup.com/t/rn68an

--- web/app/themes/northeasternUniversity/parts/single/event/event-content.php
+++ web/app/themes/northeasternUniversity/parts/single/event/event-content.php
@@ -2,7 +2,7 @@
 $event_id = get_queried_object_id();
 ?>
 <div class="event-content">
-    <?php echo do_shortcode( get_the_content( null, false, $event_id ) ); ?>
+    <?php the_content(null, false, $event_id); ?>
 </div>
 
 
