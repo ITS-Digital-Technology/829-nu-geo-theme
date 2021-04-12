2ee37bd6ef1774536e263ccb944ed809f745ef7f
Chris <therstyle@gmail.com>
2021-04-12 12:31:06 -0400

    Added a helper class to detect featured-images

--- web/app/themes/northeasternUniversity/single.php
+++ web/app/themes/northeasternUniversity/single.php
@@ -10,9 +10,10 @@
 get_header();
 the_post();
 $type = get_post_type();
+$thumbnail_class = has_post_thumbnail() ? 'has-thumbnail' : 'no-thumbnail';
 ?>
 
-<main class="page-content page-content--single page-content--<?php echo $type;?>">
+<main class="page-content page-content--single page-content--<?php echo $type;?> <?php echo $thumbnail_class; ?>">
 <?php
 
     switch ( $type ) {
