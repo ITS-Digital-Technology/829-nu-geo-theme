fe5e8d6dfb2bdbe672f19464b2746258da52677d
Chris <therstyle@gmail.com>
2021-04-28 14:41:14 -0400

    Updated aria labels

--- web/app/themes/northeasternUniversity/parts/header/header-top.php
+++ web/app/themes/northeasternUniversity/parts/header/header-top.php
@@ -3,7 +3,7 @@ $htb_logo = get_field('htb_logo', 'options');
 $htb_link = get_field('htb_link', 'options');
 ?>
 <div class="main-header__top topbar">
-    <a href="<?php echo $htb_link['url']; ?>" class="topbar__logo" aria-label="Logo">
+    <a href="<?php echo $htb_link['url']; ?>" class="topbar__logo" aria-label="<?php echo $htb_link['title']; ?>">
         <?php echo f_img($htb_logo, 'mother-logo'); ?>
     </a>
     <?php //get_theme_part('header/header-sub-nav'); ?>
