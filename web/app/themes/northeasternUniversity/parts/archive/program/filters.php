51e58524fc6bfd46b5152292fc47c309f2f314b6
Chris <therstyle@gmail.com>
2021-04-28 09:46:46 -0400

    Update list of hardcoded filters for preselect - https://app.activecollab.com/178948/projects/1727/tasks/251236

--- web/app/themes/northeasternUniversity/parts/archive/program/filters.php
+++ web/app/themes/northeasternUniversity/parts/archive/program/filters.php
@@ -3,15 +3,29 @@ $types           = isset( $_GET['p_type'] ) ? array_map('intval', explode(',', $
 $countries       = isset( $_GET['p_country'] ) ? array_map('intval', explode(',', $_GET['p_country'])) : [];
 $terms           = isset( $_GET['p_term'] ) ? array_map('intval', explode(',', $_GET['p_term'])) : [];
 $fields_of_study = isset( $_GET['p_field_of_study'] ) ? array_map('intval', explode(',', $_GET['p_field_of_study'])) : [];
+$program_tracks = isset( $_GET['p_program_track'] ) ? array_map('intval', explode(',', $_GET['p_program_track'])) : [];
+$cities = isset( $_GET['p_city'] ) ? array_map('intval', explode(',', $_GET['p_city'])) : [];
+$regions = isset( $_GET['p_region'] ) ? array_map('intval', explode(',', $_GET['p_region'])) : [];
+$class_types = isset( $_GET['p_class_type'] ) ? array_map('intval', explode(',', $_GET['p_class_type'])) : [];
+
 
 $obj = (object) [
-    'program_type' =>$types,
+    'program_type' => $types,
     'country' => $countries,
-    'term' =>$terms,
-    'field_of_study' =>$fields_of_study
+    'term' => $terms,
+    'field_of_study' => $fields_of_study,
+    'program_track' => $program_tracks,
+    'city' => $cities,
+    'region' => $regions,
+    'class_type' => $class_types,
 ];
 
 $JSON =  json_encode($obj);
+
+//TODO - work with any query string
+// $selections = [];
+// $query_string = $_SERVER['QUERY_STRING'];
+// $queries = explode('&', $query_string);
 
 $preselect = get_post_type() === 'program' ? "data-pre-select='$JSON'" : null;
 ?>
