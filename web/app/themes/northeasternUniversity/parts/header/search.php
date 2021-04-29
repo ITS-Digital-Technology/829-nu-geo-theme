85b270e3990ce7393f57e64a6196d0ad3a0a15b7
Chris <therstyle@gmail.com>
2021-04-29 08:54:40 -0400

    Moved search button to bottom for keyboard order

--- web/app/themes/northeasternUniversity/parts/header/search.php
+++ web/app/themes/northeasternUniversity/parts/header/search.php
@@ -1,8 +1,8 @@
 <div class="main-header__search search-bar">
-    <button class="search-bar__close" aria-label="Close Search">
-        <span class="icon-close"></span>
-    </button>
     <div class="search-bar__wrapper">
         <?php get_search_form(); ?>
     </div>
+    <button class="search-bar__close" aria-label="Close Search">
+        <span class="icon-close"></span>
+    </button>
 </div>
