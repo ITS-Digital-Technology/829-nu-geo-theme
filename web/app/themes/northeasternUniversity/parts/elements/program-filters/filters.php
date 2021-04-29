8f7759dc5bf1efb9c28ed95053987dc77079ed8f
Chris <therstyle@gmail.com>
2021-04-29 09:19:11 -0400

    Added aria label to program filters wrapper

--- web/app/themes/northeasternUniversity/parts/elements/program-filters/filters.php
+++ web/app/themes/northeasternUniversity/parts/elements/program-filters/filters.php
@@ -16,7 +16,7 @@ if($mobile_modal){
     <button class="program-filters__trigger-mobile"><?php _e("Search Programs",'northeasternUniversity'); ?></button>
     <div class="program-filters__wrapper-outer">
         <div class="container">
-            <div class="program-filters__wrapper">
+            <div class="program-filters__wrapper" tabindex="0" aria-label="Program Filters">
                 <div class="program-filters__inner-wrapper-header">
                     <button class="program-filters__mobile-close"><span class="icon-close"></span></button>
                 </div>
