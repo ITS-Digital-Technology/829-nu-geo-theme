78fbd5df8bb566e2190c472aaf6eac79bd865bea
Krzysztof Olszowy <krzysztof.olszowy@chop-chop.org>
2021-05-04 11:08:03 +0200

    add replace post_content_type to post_topic

--- web/app/themes/northeasternUniversity/parts/archive/post/latest-post.php
+++ web/app/themes/northeasternUniversity/parts/archive/post/latest-post.php
@@ -11,29 +11,34 @@ $posts            = $manual_selection ? $posts : get_posts( $args_posts );
 
 <section class="block-blog-latest-post">
 	<div class="container">
-        <div class="latest-post__title-wrapper">
-            <h3 class="latest-post__title"><?php echo $title; ?></h3>
-        </div>
+		<div class="latest-post__title-wrapper">
+			<h3 class="latest-post__title"><?php echo $title; ?></h3>
+		</div>
 		<?php if ( ! empty( $posts ) ) : ?>
 		<div class="row">
 			<?php
 			foreach ( $posts as $post ) :
-				$title            = get_the_title( $post );
-				$cat              = get_primary_taxonomy_term( $post, 'post_content_type' );
-				$post_author_id   = get_post_field( 'post_author', $post );
-				$thumbnail        = '';
-				$permalink        = get_permalink( $post );
+				$title          = get_the_title( $post );
+				$cat            = get_primary_taxonomy_term( $post, 'post_topic' );
+				$post_author_id = get_post_field( 'post_author', $post );
+				$thumbnail      = '';
+				$permalink      = get_permalink( $post );
 				?>
 			<div class="col-12 col-lg-3 latest-post__card-col">
-				<?php get_theme_part('archive/post/card', [
-                    'title' => $title,
-                    'cat' => $cat,
-                    'post_author_id' => $post_author_id,
-                    'thumbnail' => $thumbnail,
-                    'permalink' => $permalink
-                ]);?>
-            </div>
-            <?php endforeach; ?>
+				<?php
+				get_theme_part(
+					'archive/post/card',
+					[
+						'title'          => $title,
+						'cat'            => $cat,
+						'post_author_id' => $post_author_id,
+						'thumbnail'      => $thumbnail,
+						'permalink'      => $permalink,
+					]
+				);
+				?>
+			</div>
+			<?php endforeach; ?>
 		</div>
 		<?php endif; ?>
 	</div>
--- web/app/themes/northeasternUniversity/parts/page/block-blog-news.php
+++ web/app/themes/northeasternUniversity/parts/page/block-blog-news.php
@@ -23,89 +23,89 @@ $posts = $blog_manual_posts ? $blog_manual_posts : get_posts( $args_posts );
 $news  = $news_manual_selection ? $news_manual_posts : $query_news->posts;
 ?>
 <section class="block-blog-news"<?php ContentBlock::the_block_id(); ?>>
-    <div class="block-blog-news-wrapper">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-7">
-                    <div class="blog-posts">
-                        <div class="blog-posts__title-wrapper">
-                            <h2 class="blog-posts__title"><?php echo $blog_title; ?></h2>
-                            <a class="c-btn c-btn-tertiary c-btn-color-normal" href="<?php echo get_post_type_archive_link( 'post' ); ?>">
-                            <span><?php echo $blog_button_text; ?></span><span class="c-btn-icon"><span class="icon-arrow-right-circle"></span></span></a>
-                        </div>
-                        <div class="blog-posts__posts-wrapper">
-                            <?php
-                            foreach ( $posts as $post ) :
-                                $title            = get_the_title( $post );
-                                $cat              = get_primary_taxonomy_term( $post, 'category' );
-                                $post_author_id   = get_post_field( 'post_author', $post );
-                                $post_author_link = get_author_posts_url( $post_author_id );
-                                $author           = get_the_author_meta( 'display_name', $post_author_id );
-                                $thumbnail        = get_the_post_thumbnail( $post, 'post-card' );
-                                if ( empty( $thumbnail ) ) {
-                                    $thumbnail = false;
+	<div class="block-blog-news-wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-lg-7">
+					<div class="blog-posts">
+						<div class="blog-posts__title-wrapper">
+							<h2 class="blog-posts__title"><?php echo $blog_title; ?></h2>
+							<a class="c-btn c-btn-tertiary c-btn-color-normal" href="<?php echo get_post_type_archive_link( 'post' ); ?>">
+							<span><?php echo $blog_button_text; ?></span><span class="c-btn-icon"><span class="icon-arrow-right-circle"></span></span></a>
+						</div>
+						<div class="blog-posts__posts-wrapper">
+							<?php
+							foreach ( $posts as $post ) :
+								$title            = get_the_title( $post );
+								$cat              = get_primary_taxonomy_term( $post, 'post_topic' );
+								$post_author_id   = get_post_field( 'post_author', $post );
+								$post_author_link = get_author_posts_url( $post_author_id );
+								$author           = get_the_author_meta( 'display_name', $post_author_id );
+								$thumbnail        = get_the_post_thumbnail( $post, 'post-card' );
+								if ( empty( $thumbnail ) ) {
+									$thumbnail = false;
 
-                                }
-                                $permalink = get_permalink( $post );
-                                ?>
-                                <article class="post-card">
-                                    <a class="post-card__link" href="<?php echo $permalink; ?>" aria-label="<?php echo $title; ?>"></a>
-                                    <div class="post-card__wrapper">
-                                    <?php if ( ! empty( $thumbnail ) ) : ?>
-                                        <figure class="post-card__thumbnail"><?php echo $thumbnail; ?></figure>
-                                    <?php endif; ?>
-                                        <div class="post-card__content">
-                                        <?php if ( ! empty( $cat ) ) : ?>
-                                            <div class="post-card__cat">
-                                                <a class="post-card__cat-link" aria-label="<?php echo $cat['title']; ?> " href="<?php echo $cat['url']; ?>"><?php echo $cat['title']; ?></a>
-                                            </div>
-                                        <?php endif; ?>
-                                            <h3 class="post-card__title"><?php echo $title; ?></h3>
-                                            <div class="post-card__author">
-                                                <a aria-label="By <?php echo $author; ?>" href="<?php echo $post_author_link; ?>" class="post-card__author-link">
-                                                    <?php echo __( 'By', 'norheasternUniversity' ) . ' ' . $author; ?>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </article>
+								}
+								$permalink = get_permalink( $post );
+								?>
+								<article class="post-card">
+									<a class="post-card__link" href="<?php echo $permalink; ?>" aria-label="<?php echo $title; ?>"></a>
+									<div class="post-card__wrapper">
+									<?php if ( ! empty( $thumbnail ) ) : ?>
+										<figure class="post-card__thumbnail"><?php echo $thumbnail; ?></figure>
+									<?php endif; ?>
+										<div class="post-card__content">
+										<?php if ( ! empty( $cat ) ) : ?>
+											<div class="post-card__cat">
+												<a class="post-card__cat-link" aria-label="<?php echo $cat['title']; ?> " href="<?php echo $cat['url']; ?>"><?php echo $cat['title']; ?></a>
+											</div>
+										<?php endif; ?>
+											<h3 class="post-card__title"><?php echo $title; ?></h3>
+											<div class="post-card__author">
+												<a aria-label="By <?php echo $author; ?>" href="<?php echo $post_author_link; ?>" class="post-card__author-link">
+													<?php echo __( 'By', 'norheasternUniversity' ) . ' ' . $author; ?>
+												</a>
+											</div>
+										</div>
+									</div>
+								</article>
 
-                            <?php endforeach; ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-lg-4 offset-lg-1">
-                    <div class="news-posts">
-                        <div class="news-posts__title-wrapper">
-                            <h2 class="news-posts__title"><?php echo $news_title; ?></h2>
-                            <a class="c-btn c-btn-tertiary c-btn-color-normal" href="<?php echo get_post_type_archive_link( 'news' ); ?>">
-                            <span><?php echo $news_button_text; ?></span><span class="c-btn-icon"><span class="icon-arrow-right-circle"></span></span></a>
-                        </div>
-                        <div class="news-posts__posts-wrapper">
-                            <?php
-                            foreach ( $news as $post ) :
-                                $title     = get_the_title( $post );
-                                $cat       = get_primary_taxonomy_term( $post, 'news_category' );
-                                $terms     = get_the_terms( $post, 'news_category' );
-                                $permalink = get_permalink( $post );
-                                ?>
-                                <article class="news-card">
-                                    <a class="news-card__link" href="<?php echo $permalink; ?>" aria-label="<?php echo $title; ?>"></a>
-                                    <div class="news-card__wrapper">
-                                    <?php if ( ! empty( $cat ) ) : ?>
-                                        <div class="news-card__cat">
-                                            <a class="post-card__cat-link" aria-label="<?php echo $cat['title']; ?> " href="<?php echo $cat['url']; ?>"><?php echo $cat['title']; ?></a>
-                                        </div>
-                                    <?php endif; ?>
-                                        <h4 class="news-card__title"><?php echo $title; ?></h4>
-                                        <p class="news-card__date"><?php echo get_the_date( 'm/d/Y', $post ); ?></p>
-                                    </div>
-                                </article>
-                            <?php endforeach; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+							<?php endforeach; ?>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-lg-4 offset-lg-1">
+					<div class="news-posts">
+						<div class="news-posts__title-wrapper">
+							<h2 class="news-posts__title"><?php echo $news_title; ?></h2>
+							<a class="c-btn c-btn-tertiary c-btn-color-normal" href="<?php echo get_post_type_archive_link( 'news' ); ?>">
+							<span><?php echo $news_button_text; ?></span><span class="c-btn-icon"><span class="icon-arrow-right-circle"></span></span></a>
+						</div>
+						<div class="news-posts__posts-wrapper">
+							<?php
+							foreach ( $news as $post ) :
+								$title     = get_the_title( $post );
+								$cat       = get_primary_taxonomy_term( $post, 'news_category' );
+								$terms     = get_the_terms( $post, 'news_category' );
+								$permalink = get_permalink( $post );
+								?>
+								<article class="news-card">
+									<a class="news-card__link" href="<?php echo $permalink; ?>" aria-label="<?php echo $title; ?>"></a>
+									<div class="news-card__wrapper">
+									<?php if ( ! empty( $cat ) ) : ?>
+										<div class="news-card__cat">
+											<a class="post-card__cat-link" aria-label="<?php echo $cat['title']; ?> " href="<?php echo $cat['url']; ?>"><?php echo $cat['title']; ?></a>
+										</div>
+									<?php endif; ?>
+										<h4 class="news-card__title"><?php echo $title; ?></h4>
+										<p class="news-card__date"><?php echo get_the_date( 'm/d/Y', $post ); ?></p>
+									</div>
+								</article>
+							<?php endforeach; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
