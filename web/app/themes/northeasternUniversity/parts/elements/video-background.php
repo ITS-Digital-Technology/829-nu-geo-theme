3e1e1c3ffb11fc821ff27312cd991095f3afc358
Chris <therstyle@gmail.com>
2021-05-21 17:30:59 -0400

    Removed invalid attr - Attribute :webkitallowfullscreen not allowed on element.

--- web/app/themes/northeasternUniversity/parts/elements/video-background.php
+++ web/app/themes/northeasternUniversity/parts/elements/video-background.php
@@ -31,7 +31,7 @@ if ( isset( $video ) ) :
 			);
 		}
 
-		$attr  = 'webkitallowfullscreen mozallowfullscreen allowfullscreen';
+		$attr  = 'mozallowfullscreen';
 		$src   = add_query_arg( $params, $video_src );
 		$video = str_replace( $video_src, $src, $video );
 		$video = str_replace( '></iframe>', ' ' . $attr . '></iframe>', $video );
