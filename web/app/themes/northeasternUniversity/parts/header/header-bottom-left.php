eeed82a887e485e97b390c1a4e31c5e8e84cb45b
Chris <therstyle@gmail.com>
2021-06-08 11:47:08 -0400

    Updated wcag text

--- web/app/themes/northeasternUniversity/parts/header/header-bottom-left.php
+++ web/app/themes/northeasternUniversity/parts/header/header-bottom-left.php
@@ -4,7 +4,7 @@ if (!empty($main_logo)):
 ?>
 <div class="main-header__left">
     <a href="<?php echo home_url('/'); ?>" class="main-header__logo">
-        <span class="sr-only">Home</span>
+        <span class="sr-only">Northeastern University Global Experience Office Homepage</span>
         <?php echo f_img($main_logo, 'main-logo'); ?>
     </a>
 </div>
