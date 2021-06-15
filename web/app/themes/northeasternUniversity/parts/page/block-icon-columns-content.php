f49f5d15a599e429267a07a5335bddb43c24f1dc
Chris <therstyle@gmail.com>
2021-06-15 15:00:27 -0400

    Replaced headline with span

--- web/app/themes/northeasternUniversity/parts/page/block-icon-columns-content.php
+++ web/app/themes/northeasternUniversity/parts/page/block-icon-columns-content.php
@@ -26,7 +26,7 @@ $columns  = get_sub_field( 'columns' );
                                 <figure class="block-icon-columns-content__column-image"><?php echo $icon; ?></figure>
                             <?php endif; ?>   
                             <?php if ( ! empty( $title ) ) : ?>
-                                <h3 class="block-icon-columns-content__column-title"><?php echo $title; ?></h3>
+                                <span class="block-icon-columns-content__column-title h3"><?php echo $title; ?></span>
                             <?php endif; ?>   
                             <?php if ( ! empty( $description ) ) : ?>
                                 <p class="block-icon-columns-content__column-description"><?php echo $description; ?></p>
