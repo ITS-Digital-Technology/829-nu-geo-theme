09d1f3d7d7ac1d593f5202cf6a55c7a43f8104f6
Siddhant Chothe <sidnc86@gmail.com>
2021-07-29 19:51:18 +0530

    Remove aria-label="main" from <main>
    Same label as landmark type causes confusion for screen reader user.

--- web/app/themes/northeasternUniversity/page.php
+++ web/app/themes/northeasternUniversity/page.php
@@ -9,7 +9,10 @@
 
 get_header(); the_post();
 ?>
+    <!--ARIA landmark having same label as landmark type can cause confusion to screen reader users. Following will be read out as "main main landmark".
     <main class="page-content" aria-label="Main"><?php
+    -->
+    <main class="page-content"><?php
         get_theme_part('page/hero');
         defaultContent();
         ContentBlock::display_theme_blocks();
