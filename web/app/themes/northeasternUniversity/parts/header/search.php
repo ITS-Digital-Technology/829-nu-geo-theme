1c223c8124dfdca00ad78f30c379ee8769a367dd
Siddhant Chothe <sidnc86@gmail.com>
2021-07-29 13:13:27 +0530

    Fix few accessibility attributes

--- web/app/themes/northeasternUniversity/parts/header/search.php
+++ web/app/themes/northeasternUniversity/parts/header/search.php
@@ -1,4 +1,7 @@
+<!-- As the search box appears as an overlay, it is good candidate for being semantic dialog.
 <div class="main-header__search search-bar">
+-->
+<div class="main-header__search search-bar" role="dialog" aria-modal="true">
     <div class="search-bar__wrapper">
         <?php get_search_form(); ?>
     </div>
